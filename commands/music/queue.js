90cab499c14379b3fbc0d50774b9b22024452cb0
vqlion <valentinjossic@outlook.fr>
2023-04-17 21:30:33 +0200

    Finish queue command

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,25 +1,30 @@
 const { SlashCommandBuilder } = require("discord.js");
-const { getVoiceConnection } = require("@discordjs/voice");
-const fs = require("fs");
+const { readFileSync } = require('fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("queue")
         .setDescription("Check all the songs in the music queue"),
     async execute(interaction) {
+        await interaction.deferReply();
         const guildId = interaction.guildId;
-        let songList = play.getSongList(guildId);
-        console.log(songList);
+        let songList = getSongList();
+        if (!songList || !(guildId in songList) || !songList[guildId].length) return interaction.editReply("There are no songs in the queue right now.")
+        let res = "Got it! The queued songs are:\n"
+        await interaction.editReply(res);
+        for (let i = 0; i < songList[guildId].length; i++) {
+            res += `\`${i + 1}\` : \`${songList[guildId][i]}\` \n`
+        }
+
+        await interaction.editReply(res);
     },
 };
 
-function getSongList(guildId) {
-    let obj;
-    fs.readFile('song_list.json', 'utf8', function readFileCallback(err, data){
-        if (err){
-            console.log(err);
-        } else {
-        obj = JSON.parse(data);  
-    }});
-    return obj;
+function getSongList() {
+    try {
+        const data = readFileSync('song_list.json');
+        return JSON.parse(data);
+    } catch {
+        return null;
+    }
 }
