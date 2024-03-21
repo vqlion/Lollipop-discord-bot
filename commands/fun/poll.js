61fc95b7ab3edb1c0299dc49696dc781119d94de
vqlion <valentin.jossic@insa-lyon.fr>
2024-03-21 16:14:58 +0100

    Embed reply and save answers

--- commands/fun/poll.js
+++ commands/fun/poll.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits, ComponentType, ButtonBuilder, ButtonStyle, ActionRowBuilder, EmbedBuilder, userMention } = require("discord.js");
+
+var clientAvatar;
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -20,6 +22,13 @@ module.exports = {
 
         const question = interaction.options.getString("question");
         const options = interaction.options.getString("options").split(",");
+        const memberName = interaction.member.displayName;
+        const memberAvatar = interaction.member.avatarURL();
+        const memberId = interaction.member.user.tag;
+
+        clientAvatar = interaction.client.user.avatarURL();
+
+        var answers = {};
 
         if (options.length < 2) {
             interaction.reply({ content: "You need to provide at least 2 options.", ephemeral: true });
@@ -31,18 +40,48 @@ module.exports = {
                 .setCustomId(`option_${index}`)
                 .setLabel(option)
                 .setStyle(ButtonStyle.Primary);
-
+            
+            answers[`option_${index}`] = [];
             components.push(button);
         });
+
+        console.log(answers)
 
         const row = new ActionRowBuilder()
             .addComponents(components);
 
 
         const response = await interaction.reply({
-            content: `HIGHLY IMPORTANT POLL: ${question}`,
+            embeds: [generateEmbedResponseMessage(question, options, memberId, memberAvatar)],
             components: [row],
         });
 
+        const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3_600_000 });
+
+        collector.on('collect', async i => {
+            console.log(i.customId);
+            const selection = i.customId;
+            const memberVote = i.member;
+            answers[selection].push({ member: memberVote, selection: selection });
+            // await i.reply(`${i.user} has selected ${selection}!`);
+        });
     },
 };
+
+function generateEmbedResponseMessage(question, options, author, authorAvatar, ans) {
+    optionFields = [];
+    
+    options.forEach((option, index) => {
+        optionFields.push({ name: option, value: option });
+    });
+
+    const embededReply = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle(`Poll: **${question}**`)
+        .setAuthor({ name: 'Lollipop', iconURL: clientAvatar })
+        .addFields(optionFields)
+        .setTimestamp()
+        .setFooter({ text: author, iconURL: authorAvatar });
+
+    return embededReply;
+}
