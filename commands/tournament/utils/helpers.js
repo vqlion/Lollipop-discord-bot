8eca3025cfe56060e5bc85c9008fef41567e7850
vqlion <valentin.jossic@insa-lyon.fr>
2024-10-26 15:41:14 +0200

    Fix function to get a game's data

--- commands/tournament/utils/helpers.js
+++ commands/tournament/utils/helpers.js
@@ -8,9 +8,10 @@ module.exports = {
 
 async function getMatchData(matchId) {
     const url = `https://europe.api.riotgames.com/lol/match/v5/matches/EUW1_${matchId}?api_key=${riot_api_key}`
-    const response = await axios.get(url);
-    
-    if (response.status !== 200) return null;
-
-    return response.data;
+    try {
+        const response = await axios.get(url);
+        if (response.status === 200) return response.data;
+    } catch (error) {
+        return null;
+    }
 }
